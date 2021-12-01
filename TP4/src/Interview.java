02c89d3c4f10dcbae6764498ce3e665cf13c3b9a
pablosepulvedasolis <77544623+pablosepulvedasolis@users.noreply.github.com>
2021-12-01 14:48:58 -0500

    Interview

--- TP4/src/Interview.java
+++ TP4/src/Interview.java
@@ -2,13 +2,22 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Interview {
-
+        // Complexité : O(log(n))
         public int lastBox(int[] boxes){
 
             // Ne pas modifier la ligne suivante
             PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
-
-            return -1;
+            for(int i = 0; i < boxes.length; ++i) {
+                heap.add(boxes[i]);
+            }
+            while(heap.size() > 1) {
+                int box1 = heap.remove();
+                int box2 = heap.remove();
+                if (box1 != box2) {
+                    heap.add(box1 - box2);
+                }
+            }
+            return heap.isEmpty() ? 0 : heap.remove();
         }
 }
 
