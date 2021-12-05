226ca05ffcd85f27233c3a9fe50eb5045188a1f2
pablosepulvedasolis <77544623+pablosepulvedasolis@users.noreply.github.com>
2021-12-04 23:50:59 -0500

    partie 4

--- TP5_INF2010/src/Interview.java
+++ TP5_INF2010/src/Interview.java
@@ -22,18 +22,20 @@ public class Interview {
             Cell curr = heap.poll();
             int x = curr.xPos, y = curr.yPos, cost = curr.cost;
 
-            if (false /*TODO Condition si on arrive à la fin de la matrice */)
+            if (x == m-1 && y == n-1 /*TODO Condition si on arrive à la fin de la matrice */)
                 return cost;
-
 
             for (int i = 0; i < 4; i++) {
                 int[] dir = DIRECTIONS[i];
                 int newX = x + dir[1], newY = y + dir[0];
-                if (false/*TODO Condition qui assure qu'on est toujours dans les bornes de la matrice */) continue;
+                if (newX < 0 || newX >= m || newY < 0 || newY >= n/*TODO Condition qui assure qu'on est toujours dans les bornes de la matrice */) continue;
 
-                int newCost = -1/*TODO Calculer le nouveau cout selon le deplacement*/;
+                int newCost = cost + ((grid[x][y].cost - 1 != i ? 1 : 0))/*TODO Calculer le nouveau cout selon le deplacement*/;
+
                 if (costs[newY][newX].cost > newCost) {
                     /*TODO Mettre le nouveau cout au bonne emplacement dans la matrice & l'ajouter au heap.*/
+                    costs[newY][newX].cost = newCost;
+                    heap.offer(new Cell(newX, newY, newCost));
                 }
             }
         }
